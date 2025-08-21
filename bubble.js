e147e7eefc78183fb83160b58fda28a4744ec0bb
aracope <156398622+aracope@users.noreply.github.com>
2025-08-21 15:10:24 -0600

    Passing bubble sort function.

--- bubble.js
+++ bubble.js
@@ -1,3 +1,21 @@
-function bubbleSort() {}
+function bubbleSort(arr) {
+  let n = arr.length;
+  let swapped;
+
+  do {
+    swapped = false;
+    for (let i = 0; i < n - 1; i++) {
+      if (arr[i] > arr[i + 1]) {
+        // Swap values
+        [arr[i], arr[i + 1]] = [arr[i + 1], arr[i]];
+        swapped = true;
+      }
+    }
+    // Optimization: the last element in each pass is already sorted
+    n--;
+  } while (swapped);
+
+  return arr;
+}
 
 module.exports = bubbleSort;

bcc80adf7cbae9f6cdf3d8ed8ce935b15094b1a2
aracope <156398622+aracope@users.noreply.github.com>
2025-08-21 15:22:08 -0600

    Complete and passing insert sort funcntion.

--- insertion.js
+++ insertion.js
@@ -1,3 +1,18 @@
-function insertionSort() {}
+function insertionSort(arr) {
+  for (let i = 1; i < arr.length; i++) {
+    let currentVal = arr[i];
+    let j = i - 1;
+
+    // shift elements in the sorted portion to the right
+    while (j >= 0 && arr[j] > currentVal) {
+      arr[j + 1] = arr[j];
+      j--;
+    }
+
+    // insert currentVal in its correct spot
+    arr[j + 1] = currentVal;
+  }
+  return arr;
+}
 
 module.exports = insertionSort;
